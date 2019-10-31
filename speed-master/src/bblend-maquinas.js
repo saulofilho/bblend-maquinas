38cabfa321a592d680848bc2639447029b3c5a93
SauloFilho <saulosilvaf@gmail.com>
2019-10-31 20:08:50 -0300

    up
    thu 31 oct 20h08

--- speed-master/src/bblend-maquinas.js
+++ speed-master/src/bblend-maquinas.js
@@ -1,35 +1,74 @@
 'use strict';
 
-//btns smart
+//btn sandwich header
+function btnSandwich(x) {
+  x.classList.toggle("change");
+};
+
+//btns smart container
 $('.btns').click(function() {
 	$('#content .container-fluid').hide();
     let target = '#' + $(this).data('target');
     $(target).show();
 });
 
-//swiper carousel desktop
-let swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 30,
+//btns produtos container
+$('.btns-prod').click(function() {
+	$('#content-produtos .swiper-container').hide();
+    let target = '#' + $(this).data('target');
+    $(target).show();
+});
+
+//swiper carousel produtos
+let swiperSuco = new Swiper('.suco-fruta-desk', {
+  effect: 'coverflow',
   grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 5,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 120,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    bullets: true,
   },
 });
 
+let swiperSucoFun = new Swiper('.suco-funcional-desk', {
+  observer: true,
+  observeParents: true,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 5,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 120,
+    depth: 300,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    bullets: true,
+  },
+});
+
+$('.helperComplement').remove();
+
 //slick carousel enjoy
 $(document).ready(function() {
-  $('.prateleira ul').slick({
+  $('.shelf-bblend-maquinas ul').slick({
     dots: true,
-    infinite: false
-  });
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    infinite: true,
+  });  
 });
 
 //accordion button arrow
